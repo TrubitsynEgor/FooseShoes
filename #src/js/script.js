55308f8a2f58604730657cab16b728b1af55213b
TrubitsynEgor <trubitsynwork74@gmail.com>
2022-07-10 20:47:44 +0500

    add full page, +full adaptive+adapt-menu+searchfrm

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,5 @@
+
+
 function testWebP(callback) {
 	let webP = new Image();
 	webP.onload = webP.onerror = function () {
@@ -47,7 +49,9 @@ $(document).ready(function () {
 		$('.slider__nav-item_5').addClass('active');
 		$('.slider__nav-item_2,.slider__nav-item_1,.slider__nav-item_3,.slider__nav-item_4').removeClass('active');
 	})
-
+	// $('.header__search-btn').click(function (event) {
+	// 	$('.header__search-btn, .header__search-form').toggleClass('active');
+	// })
 });
 
 
@@ -66,14 +70,35 @@ s.forEach(ss => {
 
 
 
+
+
 const mySwiper = new Swiper('.swiper', {
 	loop: true,
-	allowTouchMove: false
+	allowTouchMove: false,
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			allowTouchMove: true,
+		},
+		// when window width is >= 480px
+		480: {
+			allowTouchMove: true,
+		},
+		// when window width is >= 640px
+		641: {
+			slidesPerView: 1,
+			allowTouchMove: true,
+		},
+		961: {
+			allowTouchMove: false,
+		}
+	},
 })
 
 const sliderBtnOne = document.getElementById('go-to-1').addEventListener('click', () => {
 	mySwiper.slideTo(1);
 });
+
 const sliderBtnTwo = document.getElementById('go-to-2').addEventListener('click', () => {
 	mySwiper.slideTo(2);
 });
@@ -89,3 +114,28 @@ const sliderBtnFive = document.getElementById('go-to-5').addEventListener('click
 
 
 
+const searchBtn = document.querySelector('.header__search-btn')
+const searchInput = document.querySelector('.header__search-form')
+const mainLogo = document.querySelector('.header__logo')
+const formLogin = document.querySelector('.header__log-form')
+
+searchBtn.addEventListener('click', (e) => {
+	if (e.target.closest('.header__search-btn')) {
+		searchBtn.classList.toggle('active');
+		searchInput.classList.toggle('active');
+		mainLogo.classList.toggle('active');
+		formLogin.classList.toggle('active');
+	}
+});
+
+
+const burgerBtn = document.querySelector('.header__burger')
+const menuList = document.querySelector('.menu__list')
+
+burgerBtn.addEventListener('click', (e) => {
+	if (e.target.closest('.header__burger')) {
+		burgerBtn.classList.toggle('active');
+		menuList.classList.toggle('active');
+		document.body.classList.toggle('lock');
+	}
+});
